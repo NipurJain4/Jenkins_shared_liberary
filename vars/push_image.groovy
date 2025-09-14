8867209c064175b820e9f7643b2e189e3f0a9ca2
Nipur Jain <104438875+NipurJain4@users.noreply.github.com>
2025-09-14 16:57:05 +0530

    Update push_image.groovy

--- vars/push_image.groovy
+++ vars/push_image.groovy
@@ -1,4 +1,7 @@
 def call(ecr_uri){
-    sh "docker push ${ecr_uri}"
+    sh '''
+    image_tag=v_${GIT_COMMIT}_${BUILD_ID}
+    docker push ${ecr_uri}:image_tag
+    '''
     echo "✅ Image pushed to ECR"
 }
