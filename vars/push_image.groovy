a47e2e8fe0347c3bceabaee06d028b311f98a60f
Nipur Jain <104438875+NipurJain4@users.noreply.github.com>
2025-09-14 17:11:16 +0530

    Update push_image.groovy

--- vars/push_image.groovy
+++ vars/push_image.groovy
@@ -1,7 +1,7 @@
 def call(ECR_URI){
     sh '''
     export image_tag=v_${GIT_COMMIT}_${BUILD_ID}
-    aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 905302884938.dkr.ecr.ap-south-1.amazonaws.com
+    #aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 905302884938.dkr.ecr.ap-south-1.amazonaws.com
     docker push ${ECR_URI}:${image_tag}
     '''
     echo "✅ Image pushed to ECR"
