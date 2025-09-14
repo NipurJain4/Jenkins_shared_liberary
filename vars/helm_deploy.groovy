48de06f3086b3ee556e815bae8b6576d579fb9bd
Nipur Jain <104438875+NipurJain4@users.noreply.github.com>
2025-09-14 19:09:26 +0530

    Update helm_deploy.groovy

--- vars/helm_deploy.groovy
+++ vars/helm_deploy.groovy
@@ -1,15 +1,37 @@
+// def call(String helmRepoUrl, String gitUser = "NipurJain4", String gitEmail = "nipurjain.tmu.cs@gmail.com") {
+//    sh"""
+//    export image_tag=v_${GIT_COMMIT}_${BUILD_ID}
+//    git clone ${helmRepoUrl}
+//    cd DevOps-Task-Swayatt-helm_chart
+//    sed -i 's/^  tag:.*$/  tag: "${image_tag}"/' values.yaml
+//     # Configure git
+//         git config user.name "${gitUser}"
+//         git config user.email "${gitEmail}"
+//           # Commit and push the change
+//         git add values.yaml
+//         git commit -m "Update Docker image tag to ${image_tag}"
+//         git push origin main
+//    """
+// }
 def call(String helmRepoUrl, String gitUser = "NipurJain4", String gitEmail = "nipurjain.tmu.cs@gmail.com") {
-   sh"""
-   export image_tag=v_${GIT_COMMIT}_${BUILD_ID}
-   git clone ${helmRepoUrl}
-   cd DevOps-Task-Swayatt-helm_chart
-   sed -i 's/^  tag:.*$/  tag: "${image_tag}"/' values.yaml
-    # Configure git
+    withCredentials([string(credentialsId: 'nipur-ssh-key', variable: 'GITHUB_TOKEN')]) {
+        sh """
+        export image_tag=v_${GIT_COMMIT}_${BUILD_ID}
+
+        # Clone repo using token
+        git clone https://${GITHUB_TOKEN}@${helmRepoUrl.replace('https://', '')}
+        cd DevOps-Task-Swayatt-helm_chart
+
+        # Update values.yaml with new image tag
+        sed -i 's/^  tag:.*$/  tag: "'"\${image_tag}"'"/' values.yaml
+
+        # Configure git
         git config user.name "${gitUser}"
         git config user.email "${gitEmail}"
-          # Commit and push the change
+
         git add values.yaml
-        git commit -m "Update Docker image tag to ${image_tag}"
+        git commit -m "Update Docker image tag to \${image_tag}" || echo "No changes to commit"
         git push origin main
-   """
+        """
+    }
 }
